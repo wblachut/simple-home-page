0410c39b329a4ef138bbbf7823748d599cb98482
Wojciech Błachut <38980771+wblachut@users.noreply.github.com>
2023-08-03 11:20:11 +0200

    add logo loader

--- src/components/common/loader.tsx
+++ src/components/common/loader.tsx
@@ -1,3 +1,20 @@
+import Logo from '~/assets/logo.svg';
+import { globalStyles } from '~/global/styles';
+
 export const Loader = () => {
-  return <span>Loading ...</span>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: globalStyles.color.lightBg,
+        maskImage: `url(${Logo})`,
+      }}
+    >
+      <img src={Logo} alt='Logo' style={{ width: '10rem' }} />
+    </div>
+  );
 };
